8f74278027539be4dcfe37a451fb91eb248f9f58
EmiksGitHub <64738281+EmiksGitHub@users.noreply.github.com>
2021-06-03 10:58:25 +0300

    Added company pages

--- src/main/java/com/course/autopodborplatform/controllers/MainController.java
+++ src/main/java/com/course/autopodborplatform/controllers/MainController.java
@@ -1,24 +1,38 @@
 package com.course.autopodborplatform.controllers;
 
 import com.course.autopodborplatform.models.Company;
-import com.course.autopodborplatform.repositories.CompanyRepositoriyI;
+import com.course.autopodborplatform.repositories.CompanyRepositoryI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class MainController {
 
     @Autowired
-    private CompanyRepositoriyI companyRepositoriyI;
+    private CompanyRepositoryI companyRepositoryI;
 
     @GetMapping("/")
     public String home(Model model) {
-        Iterable<Company> companies = companyRepositoriyI.findAll();
+        Iterable<Company> companies = companyRepositoryI.findAll();
 //        model.addAttribute("title", "Главная страница");
         model.addAttribute("companies", companies);
         return "home";
+    }
+
+    @GetMapping("/company/{id}")
+    public String company(@PathVariable(value = "id") long id, Model model) {
+        Company companies = new Company();
+        Optional<Company> company = companyRepositoryI.findById(id);
+        ArrayList<Company> res = new ArrayList<>();
+        company.ifPresent(res::add);
+        model.addAttribute("company", res);
+        return "company";
     }
 
 }
--- src/main/java/com/course/autopodborplatform/repositories/CompanyRepositoryI.java
+++ src/main/java/com/course/autopodborplatform/repositories/CompanyRepositoryI.java
@@ -3,6 +3,6 @@ package com.course.autopodborplatform.repositories;
 import com.course.autopodborplatform.models.Company;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CompanyRepositoriyI extends CrudRepository<Company, Long> {
+public interface CompanyRepositoryI extends CrudRepository<Company, Long> {
 
 }
